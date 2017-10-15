f5320069f36f7f646e6359f4c1ee924140f13a6f
Muhammad Jaffar <muhammadjaffar14@gmail.com>
2017-10-15 16:19:01 -0400

    able to add as many course items as the user wants

--- Mark Calculator/ItemTestVC.swift
+++ Mark Calculator/ItemTestVC.swift
@@ -5,45 +5,91 @@
 //  Created by Muhammad  on 2017-10-11.
 //  Copyright © 2017 Muhammad . All rights reserved.
 //
-
 import UIKit
 
 class ItemTestVC: UIViewController {
-
-//    @IBOutlet var testText: UITextField!
+    
+    var xCount : (Double) = 0;
+    var yCount = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     @IBAction func addBtn(_ sender: Any) {
         
-//        making a new textfield
-        let myField = UITextField.init(frame: CGRect.init(x:100, y:200, width: 100, height: 40))
-        myField.borderStyle = UITextBorderStyle.roundedRect;
-        myField.placeholder = "enter";
-        myField.autocorrectionType = UITextAutocorrectionType.no;
-        myField.keyboardType = UIKeyboardType.default;
-        myField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
-        myField.delegate = self as? UITextFieldDelegate;
-        myField.isOpaque = true
-        myField.text = "hey"
-        print(myField)
-        self.view.addSubview(myField)
+//        creating 3 textfield everytime the add button is pressed
+        if (yCount == 0) {
+//            course item textfield
+            let courseItemText1 = UITextField.init(frame: CGRect.init(x:9.5, y:195, width: 95, height: 30))
+            courseItemText1.borderStyle = UITextBorderStyle.roundedRect;
+            courseItemText1.placeholder = "enter"
+            courseItemText1.autocorrectionType = UITextAutocorrectionType.no
+            courseItemText1.keyboardType = UIKeyboardType.default
+            courseItemText1.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            courseItemText1.delegate = self as? UITextFieldDelegate
+            courseItemText1.isOpaque = true
+            self.view.addSubview(courseItemText1)
+//           creating the worth textfield
+            let worthText1 = UITextField.init(frame: CGRect.init(x:140, y:195, width: 95, height: 30))
+            worthText1.borderStyle = UITextBorderStyle.roundedRect
+            worthText1.placeholder = "enter"
+            worthText1.autocorrectionType = UITextAutocorrectionType.no
+            worthText1.keyboardType = UIKeyboardType.default
+            worthText1.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            worthText1.delegate = self as? UITextFieldDelegate
+            worthText1.isOpaque = true
+            self.view.addSubview(worthText1)
+//           creating the mark % textfield
+            let markText1 = UITextField.init(frame: CGRect.init(x:270, y:195, width: 95, height: 30))
+            markText1.borderStyle = UITextBorderStyle.roundedRect
+            markText1.placeholder = "enter"
+            markText1.autocorrectionType = UITextAutocorrectionType.no
+            markText1.keyboardType = UIKeyboardType.default
+            markText1.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            markText1.delegate = self as? UITextFieldDelegate
+            markText1.isOpaque = true
+            self.view.addSubview(markText1)
+        }
+        else {
+//            creating course textfields
+            let courseItemText = UITextField.init(frame: CGRect.init(x:Int(9.5), y:195 + yCount, width: 95, height: 30))
+            courseItemText.borderStyle = UITextBorderStyle.roundedRect;
+            courseItemText.placeholder = "course item";
+            courseItemText.autocorrectionType = UITextAutocorrectionType.no;
+            courseItemText.keyboardType = UIKeyboardType.default;
+            courseItemText.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            courseItemText.delegate = self as? UITextFieldDelegate;
+            courseItemText.isOpaque = true
+            self.view.addSubview(courseItemText)
+//            creating worth textfield
+            let worthText = UITextField.init(frame: CGRect.init(x:140, y:195 + yCount, width: 95, height: 30))
+            worthText.borderStyle = UITextBorderStyle.roundedRect;
+            worthText.placeholder = "worth"
+            worthText.autocorrectionType = UITextAutocorrectionType.no;
+            worthText.keyboardType = UIKeyboardType.default
+            worthText.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            worthText.delegate = self as? UITextFieldDelegate
+            worthText.isOpaque = true
+            self.view.addSubview(worthText)
+//            creating mark textfield
+            let markText = UITextField.init(frame: CGRect.init(x:270, y:195 + yCount, width: 95, height: 30))
+            markText.borderStyle = UITextBorderStyle.roundedRect
+            markText.placeholder = "mark"
+            markText.autocorrectionType = UITextAutocorrectionType.no
+            markText.keyboardType = UIKeyboardType.default
+            markText.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+            markText.delegate = self as? UITextFieldDelegate
+            markText.isOpaque = true
+            self.view.addSubview(markText)
+        }
+        yCount = yCount + 45
     }
     
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
