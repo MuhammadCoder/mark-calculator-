3603224959d0c0b6d1720e3f7c33862693b1782b
Muhammad Jaffar <muhammadjaffar14@gmail.com>
2017-11-01 08:36:04 -0400

    added a new view controller

---
+++ Mark Calculator/testVC.swift
@@ -0,0 +1,42 @@
+//
+//  testVC.swift
+//  Mark Calculator
+//
+//  Created by Muhammad  on 2017-11-01.
+//  Copyright © 2017 Muhammad . All rights reserved.
+//
+
+import UIKit
+
+class testVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addBtn(_ sender: Any) {
+    }
+    
+   
+    @IBAction func calBtn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func deleteBtn(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
